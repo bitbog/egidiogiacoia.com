9c3d236fd9980c73df3068233ffafd25f50fe727
Egidio Giacoia <bitbog@users.noreply.github.com>
2026-05-20 14:48:26 +0200

    Fix: set site url to custom domain and remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bitbog.github.io',
-  base: '/egidiogiacoia.com',
+  site: 'https://egidiogiacoia.com',
 });
